7bfbdc35231efed484cbe570f608e1480148bf8f
abdllhbyrktr <abdullahbayraktar@marun.edu.tr>
2017-07-15 01:11:47 +0300

    it checks now if the url is really a pdf file.

--- src/js/service.js
+++ src/js/service.js
@@ -9,26 +9,41 @@ function debug(message) {
   }
 }
 
+function checkFile(url, callback) {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function (ev) {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(req.getResponseHeader('Content-Type'));
+    }
+  };
+  req.open('GET', url, true);
+  req.send();
+}
+
 mxBrowser.onBrowserEvent = function (obj) {
   if (obj.type === 'ON_NAVIGATE') {
     debug(obj.url);
-    var urlExt = obj.url.split('.').pop();
-    if (urlExt === 'pdf') {
-      mxStorage.setConfig('pdf-file', obj.url);
-      var pdfUrl = 'mxaddon-pkg://{d0e723de-bdd9-4c64-95c6-c1dc9ce289f1}/window.html';
-      // close current tab and an open a window.
-      var mxTabs = mxRuntime.create('mx.browser.tabs');
-      var currentTab = mxTabs.getTabById(obj.id);
-      mxTabs.newTab({
-        url: pdfUrl
+    if (obj.url.indexOf('.pdf') > -1) {
+      checkFile(obj.url, function(contentType) {
+        //console.log(contentType);
+        if (contentType.indexOf('application/pdf') > -1) {
+          mxStorage.setConfig('pdf-file', obj.url);
+          var pdfUrl = 'mxaddon-pkg://{d0e723de-bdd9-4c64-95c6-c1dc9ce289f1}/window.html';
+          // close current tab and an open a window.
+          var mxTabs = mxRuntime.create('mx.browser.tabs');
+          var currentTab = mxTabs.getTabById(obj.id);
+          mxTabs.newTab({
+            url: pdfUrl
+          });
+          currentTab.close();
+        }
       });
-      currentTab.close();
     }
   }
-}
+};
 
 mxRuntime.onAppEvent = function (obj) {
   if (obj.type === 'M_OPEN') {
     debug(obj);
   }
-}
+};
