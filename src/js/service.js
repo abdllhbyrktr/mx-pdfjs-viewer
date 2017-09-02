1d79a7668643031e62462d1d267675157047e277
abdllhbyrktr <abdullahbayraktar@marun.edu.tr>
2017-09-03 02:29:05 +0300

    fetch api used to check if the url is a pdf file. and local file is now opened in this extension.

--- src/js/service.js
+++ src/js/service.js
@@ -1,7 +1,8 @@
 var mxRuntime = window.external.mxGetRuntime(),
   mxBrowser = mxRuntime.create('mx.browser'),
   mxStorage = mxRuntime.storage,
-  debugMode = false;
+  debugMode = false,
+  pdfUrl = 'mxaddon-pkg://{d0e723de-bdd9-4c64-95c6-c1dc9ce289f1}/window.html';
 
 function debug(message) {
   if (debugMode) {
@@ -9,35 +10,37 @@ function debug(message) {
   }
 }
 
-function checkFile(url, callback) {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function (ev) {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(req.getResponseHeader('Content-Type'));
-    }
-  };
-  req.open('GET', url, true);
-  req.send();
+function loadPdf(url, tabId) {
+  mxStorage.setConfig('pdf-file', url);
+  // close current tab and an open a window.
+  var mxTabs = mxRuntime.create('mx.browser.tabs');
+  var currentTab = mxTabs.getTabById(tabId);
+  mxTabs.newTab({
+    url: pdfUrl
+  });
+  currentTab.close();
 }
 
 mxBrowser.onBrowserEvent = function (obj) {
   if (obj.type === 'ON_NAVIGATE') {
     debug(obj.url);
+    if (obj.url === pdfUrl) {
+      return;
+    }
+
     if (obj.url.indexOf('.pdf') > -1) {
-      checkFile(obj.url, function(contentType) {
-        //console.log(contentType);
-        if (contentType.indexOf('application/pdf') > -1) {
-          mxStorage.setConfig('pdf-file', obj.url);
-          var pdfUrl = 'mxaddon-pkg://{d0e723de-bdd9-4c64-95c6-c1dc9ce289f1}/window.html';
-          // close current tab and an open a window.
-          var mxTabs = mxRuntime.create('mx.browser.tabs');
-          var currentTab = mxTabs.getTabById(obj.id);
-          mxTabs.newTab({
-            url: pdfUrl
-          });
-          currentTab.close();
-        }
-      });
+      if (obj.url.indexOf('file:///') === 0) {
+        loadPdf(obj.url, obj.id);
+      } else {
+        fetch(obj.url).then(function (res) {
+          var contentType = res.headers.get('Content-Type');
+          if (contentType && contentType.indexOf('application/pdf') > -1) {
+            loadPdf(obj.url, obj.id);
+          }
+        }).catch(function (err) {
+          console.error(err);
+        });
+      }
     }
   }
 };
